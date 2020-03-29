ccbb7ecdced6095832ddb737d578cc6469dfa696
Gustavo Adriano <jetsfloyd@gmail.com>
2020-03-29 01:57:56 -0300

    Exclui a pasta 'view' e criei as pastas usadas no flask

---
+++ flask/app/static/js/registerjs.js
@@ -0,0 +1,23 @@
+/** Estou tentando validar só letras */
+validarLetras = function ApenasLetras(e, t) {
+    try {
+        if (window.event) {
+            var charCode = window.event.keyCode;
+        } else if (e) {
+            var charCode = e.which;
+        } else {
+            return true;
+        }
+        if (
+            (charCode > 64 && charCode < 91) || 
+            (charCode > 96 && charCode < 123) ||
+            (charCode > 191 && charCode <= 255) // letras com acentos
+        ){
+            return true;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        alert(err.Description);
+    }
+}
