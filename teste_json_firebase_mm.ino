bad75311393e9509eeaa20f8fc84144a9136e788
Gustavo <53908146+GustavoAdriano@users.noreply.github.com>
2020-08-20 13:19:04 -0300

    adicionado sensor de pulso

--- teste_json_firebase_mm.ino
+++ teste_json_firebase_mm.ino
@@ -8,7 +8,9 @@
 #include "WiFiEsp.h"
 #include <Wire.h>
 #include <MPU6050_tockn.h> //biblioteca para tratamento de dados do acelerometro/giroscopio
-#include <movingAvg.h>
+//biblioteca usada para medir o BPM
+//#define USE_ARDUINO_INTERRUPTS true
+//#include <PulseSensorPlayground.h>
 
 // Emulate Serial1 on pins 6/7 if not present
 #ifndef HAVE_HWSERIAL1
@@ -16,22 +18,12 @@
 SoftwareSerial Serial1(6, 7); // RX, TX
 #endif
 
+//define o mpu6050
 MPU6050 mpu6050(Wire);
+//define o sensor de BPM
+//PulseSensorPlayground pulseSensor;
 
 float accX, accY, accZ, girX, girY, girZ; //variáveis para os eixos
-
-//======MEDIA MOVEL=====
-
-movingAvg movingAverageFilterAX(32);
-movingAvg movingAverageFilterAY(32);
-movingAvg movingAverageFilterAZ(32);
-
-movingAvg movingAverageFilterGX(32);
-movingAvg movingAverageFilterGY(32);
-movingAvg movingAverageFilterGZ(32);
-
-//======MEDIA MOVEL=====
-
 
 char ssid[] = "CLEANNET-LUCIANA";//SSID 
 char pass[] = "12345678";        // Password
@@ -42,8 +34,7 @@ char server[] = "192.168.0.104";
 // Initialize the Ethernet client object
 WiFiEspClient client;
 
-void setup()
-{
+void setup(){
   // initialize serial for debugging
   Serial.begin(9600);
   // initialize serial for ESP module
@@ -54,14 +45,6 @@ void setup()
   Wire.begin();
   //inicia o MPU6050
   mpu6050.begin();
-
-  movingAverageFilterAX.begin();
-  movingAverageFilterAY.begin();
-  movingAverageFilterAZ.begin();
-
-  movingAverageFilterGX.begin();
-  movingAverageFilterGY.begin();
-  movingAverageFilterGZ.begin();
 
   // check for the presence of the shield
   if (WiFi.status() == WL_NO_SHIELD) {
@@ -81,26 +64,37 @@ void setup()
   // you're connected now, so print out the data
   Serial.println("Conectado!");
 
-  printWifiStatus();
-
   //calcula a calibracao do giroscopio
   mpu6050.calcGyroOffsets(true);
 
-  Serial.println();
-  
+  //inicia o sensor de BPM
+//  pulseSensor.analogInput(PulseWire);   
+//  pulseSensor.blinkOnPulse(LED13);
+//  pulseSensor.setThreshold(Threshold);    
+//  if (pulseSensor.begin()) {
+//    Serial.println("We created a pulseSensor Object !");
+//  }
 }
 
 void loop()
 {
-   mpu6050.update();
+  mpu6050.update();
 
-   //pega todos os valores de eixo
-   accX = movingAverageFilterAX.reading(mpu6050.getAccX());
-   accY = movingAverageFilterAY.reading(mpu6050.getAccY());
-   accZ = movingAverageFilterAZ.reading(mpu6050.getAccZ());
-   girX = movingAverageFilterGX.reading(mpu6050.getGyroX());
-   girY = movingAverageFilterGY.reading(mpu6050.getGyroY());
-   girZ = movingAverageFilterGZ.reading(mpu6050.getGyroZ());
+  //pega todos os valores dos eixos do MPU6050
+  accX = mpu6050.getAccX();
+  accY = mpu6050.getAccY();
+  accZ = mpu6050.getAccZ();
+  girX = mpu6050.getGyroX();
+  girY = mpu6050.getGyroY();
+  girZ = mpu6050.getGyroZ();  
+
+  //sensor de BPM
+//  int myBPM = pulseSensor.getBeatsPerMinute();
+//                                              
+//  if (pulseSensor.sawStartOfBeat()) {           
+//    Serial.print("BPM: "); 
+//    Serial.println(myBPM);
+//  }
  
   // if there are incoming bytes available
   // from the server, read them and print them
@@ -109,28 +103,36 @@ void loop()
     Serial.write(c);
   }
 
+
   if(client.connect(server, 5000)){
-    Serial.println("Conectado ao servidor");
-    
+    //Serial.println("Conectado ao servidor");
     sendPostRequest();
-
-    Serial.println("Dados enviados");
-
+    //Serial.println("Dados enviados");
     }
-    delay(1000);
+
+    //'limpa' os dados dos sensores
+    accX = 0;
+    accY = 0;
+    accZ = 0;
+    girX = 0;
+    girY = 0;
+    girZ = 0;
+
+    delay(1000);  
 }
 
 void sendPostRequest(){
-    Serial.println("Enviando dados...");
-  
-   // cria o json
-    String content = "{\"sensor\":\"gir/acc\",\"petID\":\"Jonas\",\"time\":\"0\",\"girX\":"+ String(girX) +",\"girY\":"+ String(girY) + ",\"girZ\":"+ String(girZ) + ",\"accX\":"+ String(accX) + ",\"accY\":" + String(accY)+"}";
-
+      
+    // cria o json
+    String content = "{\"sensor\":\"gir/acc\",\"petID\":\"Jonas\",\"girX\":"+ String(girX) +",\"girY\":"+ String(girY) + ",\"girZ\":"+ String(girZ) + ",\"accX\":"+ String(accX) + ",\"accY\":" + String(accY)+ ",\"accZ\":" + String(accZ)+ ",\"HR\":" + String(10)+"}";
+    //Serial.print("JSON >> "); //usado para visualizar o JSON
+    //Serial.println(content);
+    
     //envia a requisição
     client.println("POST /rawData/ HTTP/1.1");
-    client.println("Host: 192.168.100.187:5000");
+    client.println("Host: 192.168.0.104:5000");
     client.println("Accept: */*");
-    //client.println("Connection: close"); comentado devido a bad requests
+    //client.println("Connection: close"); //comentado devido a bad requests
     client.println("Content-Length: " + String(content.length()));
     client.println("Content-Type: application/json; charset=utf-8;");
     client.println();
@@ -139,20 +141,3 @@ void sendPostRequest(){
  
 }
 
-void printWifiStatus()
-{
-  // print the SSID of the network you're attached to
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your WiFi shield's IP address
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength
-  long rssi = WiFi.RSSI();
-  Serial.print("Signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
-}
