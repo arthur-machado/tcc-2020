3b8dac8285b2029e6e1d0c70191068bd21655fe1
Gustavo Adriano <jetsfloyd@gmail.com>
2021-03-21 18:10:42 -0300

    atualizado arquivos de biblioteca

---
+++ flask/app/controllers/randomforest.py
@@ -0,0 +1,68 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import pandas as pd
+import numpy as np
+
+# Classificação da atividade com Random Forest
+# [Name: scikit-learn Version: 0.24.1]
+
+def WhichActivityIs(sensorData):
+
+    activity = "indefinite"
+
+    #carrega o dataset de treino
+    df = pd.read_csv("datasetDog2_v.1_WithActivity.csv")
+
+    #defini o mapeamento
+    map_activity = {'sitting':0, 'running':1, 'walking':2, 'jumping':3, 'lying_down':4, 'stopped':5}
+
+    #aplica o mapeamento ao dataset
+    df['activity'] = df['activity'].map(map_activity)
+
+    #Seleciona as variaveis
+    features = ['absolute_deviation', 'standard_deviation', 'arithmetic_average', 'RMS', 'median', 'variance']
+
+    #Defini a variavel a ser prevista
+    feat_predicted = ['activity']
+
+    #Cria os objetos
+    X = df[features].values
+    Y = sensorData
+
+    #Defini o tamanho da base de teste (split)
+    split_test_size = 0.20
+
+    #Criando dados de treino e de teste
+    X_training, X_test, Y_training, Y_test = train_test_split(X,Y, test_size = split_test_size, random_state=42)
+
+    #Criando objeto de substituicao
+    exchange_Zero = SimpleImputer(missing_values = 0, strategy = "mean")
+
+    #Substituindo os valores iguais a zero pela media dos dados
+    X_training = exchange_Zero.fit_transform(X_training)
+    X_test = exchange_Zero.fit_transform(X_test)
+
+    Y_training = exchange_Zero.fit_transform(Y_training)
+    Y_test = exchange_Zero.fit_transform(Y_test)
+
+    #Defini o modelo de RandomForest (classificador)
+    clf = RandomForestClassifier(max_depth=50, min_samples_leaf=5, min_samples_split=10, n_estimators=40, random_state=42)
+    clf.fit(X_training, Y_training)
+
+    #Calcula a acuracia e captura o id da atividade prevista
+    result = clf.predict(Y_test[-1:])
+
+    #Defini a atividade prevista
+    if result == [0]:
+        activity = "sitting"
+    elif result == [1]:
+        activity = "running"
+    elif result == [2]:
+       activity = "walking"
+    elif result == [3]:
+        activity = "jumping"
+    elif result == [4]:
+        activity = "lying_down"
+    elif result == [5]:
+        activity = "stopped"
